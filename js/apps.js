340abbc6f6ab749e5164b60e6737633b3bb59779
7-programacion <7programacion@gmail.com>
2026-05-05 16:30:22 -0300

    Manejo de eventos terminado

--- js/apps.js
+++ js/apps.js
@@ -1,3 +1,35 @@
 function Saludar(){
-    alert("Hola!!");
+    alert("HOLA MUNDO");
 }
+
+// Asignamos a la variable miBoton el boton del html con id btn
+let miboton = document.getElementById("btn");
+miboton.addEventListener("click", Saludar);
+
+// esta es la forma correcta de usar funciones en javascript
+
+// miboton.addEventListener("click", function() {
+//     Saludar("Hola");
+// });
+let miboton2 = document.getElementById("btn2");
+
+function cambiar_text(){
+    let titulo = document.getElementById("titulo");
+    titulo.innerText ="ESCUCHANDO NUEVOS EVENTOS";
+}
+
+miboton2.addEventListener("click", cambiar_text);
+
+let TextOriginal = document.getElementById("input");
+
+TextOriginal.addEventListener("input", function() {
+  document.getElementById("res").innerText = TextOriginal.value;
+});
+
+document.getElementById("padre").addEventListener("click", () => {
+  console.log("Padre");
+});
+
+// document.getElementById("hijo").addEventListener("click", () => {
+// //   console.log("Hijo");
+// });
