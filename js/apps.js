780f01fced1e4d896cdccad3d7255a1a37260d0d
7-programacion <7programacion@gmail.com>
2026-05-06 16:13:18 -0300

    Escuchando Eventos

--- js/apps.js
+++ js/apps.js
@@ -1,30 +1,32 @@
-// function Saludar(){
-//     alert("HOLA MUNDO");
-// }
+// ESCUCHAR EVENTO Y SALUDAR
+function Saludar(){
+    alert("HOLA MUNDO");
+}
+// Asignamos a la variable miBoton el boton del html con id btn
+let miboton = document.getElementById("btn");
+miboton.addEventListener("click", Saludar);
 
-// // Asignamos a la variable miBoton el boton del html con id btn
-// let miboton = document.getElementById("btn");
-// miboton.addEventListener("click", Saludar);
 
-// // esta es la forma correcta de usar funciones en javascript
+// CAMBIAR TITULO
+let miboton2 = document.getElementById("btn2");
 
-// // miboton.addEventListener("click", function() {
-// //     Saludar("Hola");
-// // });
-// let miboton2 = document.getElementById("btn2");
+function cambiar_text(){
+    let titulo = document.getElementById("titulo");
+    titulo.innerText ="ESCUCHANDO NUEVOS EVENTOS";
+}
+miboton2.addEventListener("click", cambiar_text);
 
-// function cambiar_text(){
-//     let titulo = document.getElementById("titulo");
-//     titulo.innerText ="ESCUCHANDO NUEVOS EVENTOS";
-// }
+// OTROS TIPOS DE EVENTOS
+let TextOriginal = document.getElementById("input");
 
-// miboton2.addEventListener("click", cambiar_text);
+function reproducir_texto (){
+document.getElementById("res").innerText = TextOriginal.value;
+};
+TextOriginal.addEventListener("input", reproducir_texto);
 
-// let TextOriginal = document.getElementById("input");
+// s
 
-// TextOriginal.addEventListener("input", function() {
-//   document.getElementById("res").innerText = TextOriginal.value;
-// });
+// EVENTOS ANIDADOS
 
 // document.getElementById("padre").addEventListener("click", () => {
 //   console.log("Padre");
@@ -36,14 +38,14 @@
 
 
 
-// 
-let consulta = document.getElementById("btn-api");
-function consumir_api()  {
-  fetch("https://api.chucknorris.io/jokes/random")
-    .then(r => r.json())
-    .then(data => {
+// // 
+// let consulta = document.getElementById("btn-api");
+// function consumir_api()  {
+//   fetch("https://api.chucknorris.io/jokes/random")
+//     .then(r => r.json())
+//     .then(data => {
         
-      document.getElementById("respuesta").innerText = data.value;
-    });
-}
-consulta.addEventListener("click",consumir_api);
+//       document.getElementById("respuesta").innerText = data.value;
+//     });
+// }
+// consulta.addEventListener("click",consumir_api);
