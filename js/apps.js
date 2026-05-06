76b0e9d1660af44d05b94fba03870db204a15499
7-programacion <7programacion@gmail.com>
2026-05-06 16:56:48 -0300

    Escuchando Eventos Terminado

--- js/apps.js
+++ js/apps.js
@@ -16,7 +16,9 @@ function cambiar_text(){
 }
 miboton2.addEventListener("click", cambiar_text);
 
+
 // OTROS TIPOS DE EVENTOS
+
 let TextOriginal = document.getElementById("input");
 
 function reproducir_texto (){
@@ -24,28 +26,13 @@ document.getElementById("res").innerText = TextOriginal.value;
 };
 TextOriginal.addEventListener("input", reproducir_texto);
 
-// s
 
-// EVENTOS ANIDADOS
+let reset = document.getElementById("reset");
 
-// document.getElementById("padre").addEventListener("click", () => {
-//   console.log("Padre");
-// });
+function reset_campos(){
+  TextOriginal.value= "";
+  document.getElementById("res").innerText="";
+  titulo.innerText="ESCUCHANDO EVENTOS";
 
-// document.getElementById("hijo").addEventListener("click", () => {
-// //   console.log("Hijo");
-// });
-
-
-
-// // 
-// let consulta = document.getElementById("btn-api");
-// function consumir_api()  {
-//   fetch("https://api.chucknorris.io/jokes/random")
-//     .then(r => r.json())
-//     .then(data => {
-        
-//       document.getElementById("respuesta").innerText = data.value;
-//     });
-// }
-// consulta.addEventListener("click",consumir_api);
+}
+reset.addEventListener("click",reset_campos);
